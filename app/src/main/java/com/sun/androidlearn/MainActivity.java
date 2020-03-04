6ec32d6e9a8fbb9ec03ff3630035c5a2597a1987
jfson <sunzhenandroid@126.com>
2020-03-04 22:37:05 +0800

    1.列表

--- app/src/main/java/com/sun/androidlearn/MainActivity.java
+++ app/src/main/java/com/sun/androidlearn/MainActivity.java
@@ -1,7 +1,12 @@
 package com.sun.androidlearn;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.sun.androidlearn.ui.ListActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);//second
+
+
+
+        // 列表
+        // ListView  很少很少
+        // RecyclerView 用的比较多
+
+
+        TextView view = findViewById(R.id.gotoListActivity);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ListActivity.class));
+            }
+        });
+
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/sun/androidlearn/ui/ListActivity.java
@@ -0,0 +1,120 @@
+package com.sun.androidlearn.ui;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.sun.androidlearn.R;
+
+import java.util.ArrayList;
+
+public class ListActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    //作业：
+    //微信列表：
+
+    /**
+     图   Name
+     图   chat
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        //test data
+        ArrayList<String> data = new ArrayList<>();
+        data.add("盗墓笔记1");
+        data.add("盗墓笔记2");
+        data.add("盗墓笔记3");
+        data.add("盗墓笔记4");
+        data.add("盗墓笔记5");
+        data.add("盗墓笔记6");
+        data.add("盗墓笔记7");
+        data.add("盗墓笔记8");
+        data.add("盗墓笔记9");
+        data.add("盗墓笔记10");
+        data.add("盗墓笔记11");
+        data.add("盗墓笔记12");
+        data.add("盗墓笔记13");
+        data.add("盗墓笔记14");
+        data.add("盗墓笔记15");
+        data.add("盗墓笔记16");
+
+        recyclerView = findViewById(R.id.recycler);
+
+        //LinearLayoutManager
+        layoutManager = new LinearLayoutManager(this);
+
+//        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,3);
+        recyclerView.setLayoutManager(layoutManager);
+
+        mAdapter = new MyAdapter(data);
+        recyclerView.setAdapter(mAdapter);
+    }
+
+
+    public void test() {
+        recyclerView = null;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView textView;
+
+        public MyViewHolder(View v) {
+            super(v);
+            textView = v.findViewById(R.id.item_text);
+        }
+
+        public void setData(String text) {
+            textView.setText(text);
+        }
+    }
+
+    //Adapter
+    public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+        private ArrayList<String> mList;
+
+        public MyAdapter(ArrayList<String> list) {
+            mList = list;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                               int viewType) {
+            // create a new view
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.list_item, parent, false);
+
+            MyViewHolder vh = new MyViewHolder(v);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            // - get element from your dataset at this position
+            // - replace the contents of the view with that element
+            holder.setData(mList.get(position));
+
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return mList.size();
+        }
+    }
+}
