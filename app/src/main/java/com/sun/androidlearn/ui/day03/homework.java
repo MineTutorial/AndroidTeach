989e784553d150b11394bfcb5a2512bb99b26c26
jfson <sunzhenandroid@126.com>
2020-04-04 21:35:53 +0800

    1. camera homework

---
+++ app/src/main/java/com/sun/androidlearn/ui/day03/homework.java
@@ -0,0 +1,12 @@
+package com.sun.androidlearn.ui.day03;
+
+/**
+ * Author: jfson sun
+ * Create on:  2020/4/4
+ * Question:
+ * Description:
+ * Train of thought:
+ */
+public class homework {
+    // 写CamerActivity:这个页面有一个图，一个按钮，点击按钮跳转相机，拍照回来，图片展示拍照的图
+}
