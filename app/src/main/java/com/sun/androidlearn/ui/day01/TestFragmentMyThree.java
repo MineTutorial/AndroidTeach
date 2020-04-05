832de9a3cdc3d71ac747569cfc66058ecc1882dc
yuniler <sunguangyuniler@163.com>
2020-04-05 20:27:30 +0800

    niuke3

---
+++ app/src/main/java/com/sun/androidlearn/ui/day01/TestFragmentMyThree.java
@@ -0,0 +1,52 @@
+package com.sun.androidlearn.ui.day01;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sun.androidlearn.R;
+
+public class TestFragmentMyThree extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle saveInstanceState){
+        return inflater.inflate(R.layout.fragment_test03, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+    }
+}
+
