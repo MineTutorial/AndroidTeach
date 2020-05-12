dc25801e99152708e055a160b5f787411bc4ec3e
jfson <sunzhenandroid@126.com>
2020-05-12 17:19:32 +0800

    1. 入口

---
+++ app/src/main/java/com/sun/androidlearn/bili/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.sun.androidlearn.bili;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sun.androidlearn.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
