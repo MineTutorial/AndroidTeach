330ca575bef797399be4f39f1606fc71347d90c6
yuniler <sunguangyuniler@163.com>
2020-05-13 19:36:25 +0800

    bili_按钮完成

--- app/src/main/java/com/sun/androidlearn/bili/HomeActivity.java
+++ app/src/main/java/com/sun/androidlearn/bili/HomeActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.sun.androidlearn.R;
 import com.sun.androidlearn.ui.day02.ViewPagerActivity;
@@ -19,6 +20,7 @@ public class HomeActivity extends AppCompatActivity {
     ConstraintLayout mBibiConstrainLayout;
     ViewPagerActivity.MyPagerAdapter myPagerAdapter;
     ImageView imageOne, imageTwo, imageThree, imageFour;
+    TextView textOne, textTwo, textThree, textFour;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,58 +43,122 @@ public class HomeActivity extends AppCompatActivity {
         imageOne = findViewById(R.id.page_one);
         imageTwo = findViewById(R.id.page_two);
         imageThree = findViewById(R.id.page_three);
-        imageFour =findViewById(R.id.page_four);
+        imageFour = findViewById(R.id.page_four);
+
+        textOne = findViewById(R.id.bili_text_one);
+        textTwo = findViewById(R.id.bili_text_two);
+        textThree = findViewById(R.id.bili_text_three);
+        textFour = findViewById(R.id.bili_text_four);
 
 //        mBibiViewPager.setCurrentItem(0);
-        imageOne.setSelected(true);
-        imageTwo.setSelected(false);
-        imageThree.setSelected(false);
-        imageFour.setSelected(false);
+//        imageOne.setSelected(true);
+//        imageTwo.setSelected(false);
+//        imageThree.setSelected(false);
+//        imageFour.setSelected(false);
 //
-
-
-        mBibiConstrainLayout.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        imageOne.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onPageScrolled(int i, float v, int i1) {
+            public void onClick(View v) {
+                imageOne.setSelected(true);
+                imageTwo.setSelected(false);
+                imageThree.setSelected(false);
+                imageFour.setSelected(false);
 
-            }
-
-            @Override
-            public void onPageSelected(int i) {
-            switch (i){
-                case 0:
-                    imageOne.setSelected(true);
-                    imageTwo.setSelected(false);
-                    imageThree.setSelected(false);
-                    imageFour.setSelected(false);
-                    break;
-                case 1:
-                    imageOne.setSelected(false);
-                    imageTwo.setSelected(true);
-                    imageThree.setSelected(false);
-                    imageFour.setSelected(false);
-                    break;
-                case 2:
-                    imageOne.setSelected(false);
-                    imageTwo.setSelected(false);
-                    imageThree.setSelected(true);
-                    imageFour.setSelected(false);
-                    break;
-                case 3:
-                    imageOne.setSelected(false);
-                    imageTwo.setSelected(false);
-                    imageThree.setSelected(false);
-                    imageFour.setSelected(true);
-                    break;
-
-                }
-            }
-            //
-
-            @Override
-            public void onPageScrollStateChanged(int i) {
-
+                textOne.setSelected(true);
+                textTwo.setSelected(false);
+                textThree.setSelected(false);
+                textFour.setSelected(false);
             }
         });
+
+
+        imageTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageOne.setSelected(false);
+                imageTwo.setSelected(true);
+                imageThree.setSelected(false);
+                imageFour.setSelected(false);
+
+                textOne.setSelected(false);
+                textTwo.setSelected(true);
+                textThree.setSelected(false);
+                textFour.setSelected(false);
+            }
+        });
+        imageThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageOne.setSelected(false);
+                imageTwo.setSelected(false);
+                imageThree.setSelected(true);
+                imageFour.setSelected(false);
+
+                textOne.setSelected(false);
+                textTwo.setSelected(false);
+                textThree.setSelected(true);
+                textFour.setSelected(false);
+            }
+        });
+
+        imageFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageOne.setSelected(false);
+                imageTwo.setSelected(false);
+                imageThree.setSelected(false);
+                imageFour.setSelected(true);
+
+                textOne.setSelected(false);
+                textTwo.setSelected(false);
+                textThree.setSelected(false);
+                textFour.setSelected(true);
+            }
+        });
+
+
+//        mBibiConstrainLayout.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+//            @Override
+//            public void onPageScrolled(int i, float v, int i1) {
+//
+//            }
+//
+//            @Override
+//            public void onPageSelected(int i) {
+//            switch (i){
+//                case 0:
+//                    imageOne.setSelected(true);
+//                    imageTwo.setSelected(false);
+//                    imageThree.setSelected(false);
+//                    imageFour.setSelected(false);
+//                    break;
+//                case 1:
+//                    imageOne.setSelected(false);
+//                    imageTwo.setSelected(true);
+//                    imageThree.setSelected(false);
+//                    imageFour.setSelected(false);
+//                    break;
+//                case 2:
+//                    imageOne.setSelected(false);
+//                    imageTwo.setSelected(false);
+//                    imageThree.setSelected(true);
+//                    imageFour.setSelected(false);
+//                    break;
+//                case 3:
+//                    imageOne.setSelected(false);
+//                    imageTwo.setSelected(false);
+//                    imageThree.setSelected(false);
+//                    imageFour.setSelected(true);
+//                    break;
+//
+//                }
+//            }
+//            //
+//
+//            @Override
+//            public void onPageScrollStateChanged(int i) {
+//
+//            }
+//        });
     }
 }
